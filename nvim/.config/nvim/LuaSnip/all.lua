d5541341fa4cdf11dfb38a7ce2154d63418c6e55
azzen <6373020+azzen@users.noreply.github.com>
2025-03-21 18:08:18 +0100

    feat(general): initial home-manager configuration

--- nvim/.config/nvim/LuaSnip/all.lua
+++
@@ -1,9 +0,0 @@
-local ls    = require('luasnip')
-local s     = ls.snippet
-local t     = ls.text_node
-local f     = ls.function_node
-local i     = ls.insert_node
-local fmt   = require('luasnip.extras.fmt').fmt
-local fmta  = require('luasnip.extras.fmt').fmta
-
-
--- nvim/.config/nvim/lua/tree-sitter-cfg.lua
+++
@@ -1,6 +0,0 @@
-require('nvim-treesitter.configs').setup({
-    ensure_installed = {'c', 'cpp', 'lua', 'latex', 'vim', 'help'},
-    highlight = {
-        enable = true,
-    },
-})
